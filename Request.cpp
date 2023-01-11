898f80927871b64c60c11d349af2511c7760e17d
Eugene <lehongthai2000@gmail.com>
2023-01-11 22:25:28 +0700

    add attributes and methods

--- Request.cpp
+++ Request.cpp
@@ -1,6 +1,7 @@
 #include "Request.h"
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
@@ -15,12 +16,20 @@ Request::Request(
     int id,
     Member occupant,
     House house,
-    bool requestStatus)
+    bool requestStatus,
+    double houseRating,
+    double occupantRating,
+    string houseReview,
+    string occupantReview)
 {
     this->id = id;
     this->occupant = occupant;
     this->house = house;
     this->requestStatus = requestStatus;
+    this->houseRating = houseRating;
+    this->occupantRating = occupantRating;
+    this->houseReview = houseReview;
+    this->occupantReview = occupantReview;
 }
 
 // GETTERS
@@ -28,11 +37,65 @@ int Request::getId() { return this->id; }
 Member Request::getOccupant() { return this->occupant; }
 House Request::getHouse() { return this->house; }
 bool Request::getRequestStatus() { return this->requestStatus; }
+double Request::getHouseRating() { return this->houseRating; }
+double Request::getOccupantRating() { return this->occupantRating; }
+string Request::getHouseReview() { return this->houseReview; }
+string Request::getOccupantReview() { return this->occupantReview; }
 
 // SETTERS
 void Request::setId(int id) { this->id = id; }
 void Request::setOccupant(Member occupant) { this->occupant = occupant; }
 void Request::setHouse(House house) { this->house = house; }
 void Request::setRequestStatus(bool status) { this->requestStatus = status; }
+void Request::setHouseRating(double houseRating) { this->houseRating = houseRating; }
+void Request::setOccupantRating(double occupantRating) { this->occupantRating = occupantRating; }
+void Request::setHouseReview(string houseReview) { this->houseReview = houseReview; }
+void Request::setOccupantReview(string occupantReview) { this->occupantReview = occupantReview; }
 
 // METHODS
+void Request::showAllRequestAttributes()
+{
+    cout << "   " << setw(10) << "request id"
+         << " | " << setw(11) << "occupant id"
+         << " | " << setw(10) << "house id"
+         << " | " << setw(15) << "request status"
+         << " | " << setw(14) << "house rating"
+         << " | " << setw(16) << "occupant rating"
+         << " | " << setw(20) << "house review"
+         << " | " << setw(15) << "occpant review\n";
+}
+
+void Request::showAllRequestInfo()
+{
+    cout << boolalpha;
+    if (this->requestStatus == false)
+    {
+        cout << "   " << setw(10) << this->id
+             << " | " << setw(11) << this->occupant.getId()
+             << " | " << setw(10) << this->house.getId()
+             << " | " << setw(15) << this->requestStatus
+             << " | " << setw(14) << ""
+             << " | " << setw(16) << ""
+             << " | " << setw(20) << ""
+             << " | " << setw(15) << ""
+             << "\n";
+    }
+
+    if (this->requestStatus == true)
+    {
+        cout << "   " << setw(10) << this->id
+             << " | " << setw(11) << this->occupant.getId()
+             << " | " << setw(10) << this->house.getId()
+             << " | " << setw(15) << this->requestStatus
+             << " | " << setw(14) << this->houseRating
+             << " | " << setw(16) << this->occupantRating
+             << " | " << setw(20) << this->houseReview
+             << " | " << setw(15) << this->occupantReview << "\n";
+    }
+}
+
+void Request::showAll()
+{
+    showAllRequestAttributes();
+    showAllRequestInfo();
+}
--- Request.h
+++ Request.h
@@ -2,11 +2,8 @@
 #include "House.h"
 #include <iostream>
 
-
 #ifndef REQUEST_H
 #define REQUEST_H
-
-
 
 class Request
 {
@@ -15,21 +12,37 @@ private:
     Member occupant = Member();
     House house = House();
     bool requestStatus = false;
+    double houseRating = 0;
+    double occupantRating = 0;
+    string houseReview = "";
+    string occupantReview = "";
 
 public:
     Request();
     ~Request();
-    Request(int id, Member occupant, House house, bool requestStatus);
+    Request(int id, Member occupant, House house, bool requestStatus, double houseRating, double occupantRating, string houseReview, string occupantReview);
 
     int getId();
     Member getOccupant();
-    House getHouse();    
+    House getHouse();
     bool getRequestStatus();
+    double getHouseRating();
+    double getOccupantRating();
+    string getHouseReview();
+    string getOccupantReview();
 
     void setId(int id);
     void setOccupant(Member occupant);
     void setHouse(House house);
     void setRequestStatus(bool status);
+    void setHouseRating(double houseRating);
+    void setOccupantRating(double occupantRating);
+    void setHouseReview(string houseReview);
+    void setOccupantReview(string occupantReview);
+
+    void showAllRequestAttributes();
+    void showAllRequestInfo();
+    void showAll();
 };
 
 #endif
