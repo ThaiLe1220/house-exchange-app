8cebd42d179e8543742e466e8f1c417ed4123458
Eugene <lehongthai2000@gmail.com>
2023-01-11 22:21:42 +0700

    simple debug

--- Admin.cpp
+++ Admin.cpp
@@ -9,7 +9,7 @@ using namespace std;
 Admin::Admin() {}
 Admin::~Admin() {}
 
-Admin::Admin(int id = 0, string username = "admin", string password = "admin") 
+Admin::Admin(int id, string username, string password) 
 {
     this->id = id;
     this->username = username;
