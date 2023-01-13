c9edc2d7870c06d980af8a9a20027b8157e0688b
Eugene <lehongthai2000@gmail.com>
2023-01-13 18:08:06 +0700

    add 2 methods for house & occupant rating, add comments

--- Data.cpp
+++ Data.cpp
@@ -7,6 +7,7 @@
 #include <iterator>
 #include <fstream>
 #include <chrono>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -31,7 +32,7 @@ void Data::setHouseList(vector<House> houseList) { this->houseList = houseList; 
 void Data::setMemberList(vector<Member> memberList) { this->memberList = memberList; }
 
 // METHODS
-void Data::addHouse(House house)
+void Data::addHouse(House house) // add new house to data: houseList
 {
     int maxId = 0;
     for (auto &h : this->houseList)
@@ -48,7 +49,7 @@ void Data::addHouse(House house)
 
     this->houseList.push_back(house);
 }
-void Data::addMember(Member member)
+void Data::addMember(Member member) // add new member to data: memberList
 {
     int maxId = 0;
     for (auto &m : this->memberList)
@@ -64,7 +65,7 @@ void Data::addMember(Member member)
     }
     this->memberList.push_back(member);
 }
-void Data::addRequest(Request request)
+void Data::addRequest(Request request) // add new request to data: requestList
 {
     int maxId = 0;
     for (auto &r : this->requestList)
@@ -81,7 +82,7 @@ void Data::addRequest(Request request)
     this->requestList.push_back(request);
 }
 
-void Data::removeHouse(House house)
+void Data::removeHouse(House house) // remove house from data: houseList
 {
     House h = House();
     vector<House> houses = this->houseList;
@@ -97,7 +98,7 @@ void Data::removeHouse(House house)
     }
     this->houseList = houses;
 }
-void Data::removeMember(Member member)
+void Data::removeMember(Member member) // remove member from data: memberList
 {
     Member m = Member();
     vector<Member> members = this->memberList;
@@ -114,7 +115,7 @@ void Data::removeMember(Member member)
     this->memberList = members;
 }
 
-void Data::removeRequest(Request request)
+void Data::removeRequest(Request request) // remove request from data: requestList
 {
     Request r = Request();
     vector<Request> requests = this->requestList;
@@ -131,7 +132,7 @@ void Data::removeRequest(Request request)
     this->requestList = requests;
 }
 
-void Data::updateHouse(House house)
+void Data::updateHouse(House house) // update house to data: houseList, add new one if not exist
 {
     bool status = false;
     for (auto &h : this->houseList)
@@ -146,7 +147,7 @@ void Data::updateHouse(House house)
     if (status == false || this->houseList.empty() == true)
         addHouse(house);
 }
-void Data::updateMember(Member member)
+void Data::updateMember(Member member) // update member to data: memberList, add new one if not exist
 {
     bool status = false;
     for (auto &m : this->memberList)
@@ -161,7 +162,7 @@ void Data::updateMember(Member member)
     if (status == false || this->memberList.empty() == true)
         addMember(member);
 }
-void Data::updateRequest(Request request)
+void Data::updateRequest(Request request) // update request to data: requestList, add new one if not exist
 {
     bool status = false;
     for (auto &r : this->requestList)
@@ -177,7 +178,7 @@ void Data::updateRequest(Request request)
         addRequest(request);
 }
 
-House Data::getHouseById(int id)
+House Data::getHouseById(int id) // return house of data: houseList by id
 {
     for (int i = 0; i < this->houseList.size(); i++)
     {
@@ -187,7 +188,7 @@ House Data::getHouseById(int id)
         }
     }
 }
-Member Data::getMemberById(int id)
+Member Data::getMemberById(int id) // return member of data: memberList by id
 {
     for (int i = 0; i < this->memberList.size(); i++)
     {
@@ -197,7 +198,7 @@ Member Data::getMemberById(int id)
         }
     }
 }
-Request Data::getRequestById(int id)
+Request Data::getRequestById(int id) // return request of data: requestList by id
 {
     for (int i = 0; i < this->requestList.size(); i++)
     {
@@ -207,7 +208,7 @@ Request Data::getRequestById(int id)
         }
     }
 }
-Member Data::getMemberByUsername(string username)
+Member Data::getMemberByUsername(string username) // return member of data: memberList by username
 {
     for (int i = 0; i < this->memberList.size(); i++)
     {
@@ -217,7 +218,7 @@ Member Data::getMemberByUsername(string username)
         }
     }
 }
-Member Data::getMemberByHouseId(int id)
+Member Data::getMemberByHouseId(int id) // return member of data: memberList by Houseid
 {
     for (int i = 0; i < this->memberList.size(); i++)
     {
@@ -227,7 +228,7 @@ Member Data::getMemberByHouseId(int id)
         }
     }
 }
-bool Data::verifyMemberByUsername(string username)
+bool Data::verifyMemberByUsername(string username) // check if member is in data: memberList with username
 {
     bool result = false;
     for (int i = 0; i < this->memberList.size(); i++)
@@ -239,7 +240,7 @@ bool Data::verifyMemberByUsername(string username)
     }
     return result;
 }
-bool Data::verifyMemberByUsernameAndPassword(string username, string password)
+bool Data::verifyMemberByUsernameAndPassword(string username, string password) // check if member is in data:memberList with username and password
 {
     bool result = false;
     for (int i = 0; i < this->memberList.size(); i++)
@@ -251,7 +252,7 @@ bool Data::verifyMemberByUsernameAndPassword(string username, string password)
     }
     return result;
 }
-bool Data::verifyHouseById(int id)
+bool Data::verifyHouseById(int id) // check if house is in data:houseList with id
 {
     bool result = false;
     for (auto &h : this->houseList)
@@ -264,7 +265,7 @@ bool Data::verifyHouseById(int id)
     return result;
 }
 
-bool Data::verifyRequestById(int id)
+bool Data::verifyRequestById(int id) // check if request is in data:requestList with id
 {
     bool result = false;
     for (auto &r : this->requestList)
@@ -277,17 +278,16 @@ bool Data::verifyRequestById(int id)
     return result;
 }
 
-Request Data::createRequestToOccupy(Member occupant, int houseId)
+Request Data::createRequestToOccupy(Member occupant, int houseId) // create new request with input occupant and houseId
 {
     Request request;
-    request.setId(this->requestList.size()); // this id thing is compliated!!! Put it here for now...
     request.setOccupant(occupant);
     request.setHouse(getHouseById(houseId));
     request.setRequestStatus(false);
     return request;
 }
 
-void Data::deleteHouseById(int id)
+void Data::deleteHouseById(int id) // delete house in data: houseList by Id
 {
     for (int i = 0; i < this->houseList.size(); i++)
     {
@@ -297,7 +297,7 @@ void Data::deleteHouseById(int id)
         }
     }
 }
-void Data::deleteRequestByHouseId(int id)
+void Data::deleteRequestByHouseId(int id) // delete request in data: requestList by Id
 {
     for (auto &r : this->requestList)
     {
@@ -308,7 +308,7 @@ void Data::deleteRequestByHouseId(int id)
     }
 }
 
-void Data::deleteOtherRequestAfterAccept(Request request, vector<Request> requests)
+void Data::deleteOtherRequestAfterAccept(Request request, vector<Request> requests) // delete all other requests in a request list, but the input request
 {
     for (auto &r : this->requestList)
     {
@@ -323,7 +323,7 @@ void Data::deleteOtherRequestAfterAccept(Request request, vector<Request> reques
     addRequest(request);
 }
 
-vector<Request> Data::deleteRequestFromRequestList(Request request, vector<Request> requests)
+vector<Request> Data::deleteRequestFromRequestList(Request request, vector<Request> requests) // return list of requests with the input request deleted
 {
 
     Request r = Request();
@@ -340,7 +340,7 @@ vector<Request> Data::deleteRequestFromRequestList(Request request, vector<Reque
     return requests;
 }
 
-void Data::ViewAllMembers()
+void Data::ViewAllMembers() // print out all members in data: memberList
 {
     Member m = Member();
     m.showAllMemberAttributes();
@@ -351,7 +351,7 @@ void Data::ViewAllMembers()
     }
 }
 
-void Data::ViewAllHouses()
+void Data::ViewAllHouses() // print out all houses in data: houseList
 {
     House h = House();
     h.showAllHouseAttributes();
@@ -362,7 +362,7 @@ void Data::ViewAllHouses()
     }
 }
 
-void Data::ViewAllRequests()
+void Data::ViewAllRequests() // print out all requests in data: requestList
 {
     Request req = Request();
     req.showAllRequestAttributes();
@@ -373,55 +373,92 @@ void Data::ViewAllRequests()
     }
 }
 
-void Data::writeDataToFile(string filename)
+void Data::writeDataToFile(string filename) // write data to file, with divided section for house, member and request, data is presented with sorted id
 {
+    vector<int> v; // use for sorting;
+
     ofstream myfile(filename);
     myfile << "[Member Data Start]" << endl;
     myfile << "id,,fullname,,username,,password,,phone,,creditPoint,,occupierRating,,houseId" << endl;
     for (auto &m : this->memberList)
     {
-
-        myfile << m.getId() << ",,"
-               << m.getFullname() << ",,"
-               << m.getUsername() << ",,"
-               << m.getPassword() << ",,"
-               << m.getPhone() << ",,"
-               << m.getCreditPoint() << ",,"
-               << m.getOccupierRating() << ",,"
-               << m.getHouse().getId() << endl;
+        v.push_back(m.getId());
+    }
+    sort(v.begin(), v.end());
+    for (auto &n : v)
+    {
+        for (auto &m : this->memberList)
+        {
+            if (n == m.getId())
+            {
+                myfile
+                    << m.getId() << ",,"
+                    << m.getFullname() << ",,"
+                    << m.getUsername() << ",,"
+                    << m.getPassword() << ",,"
+                    << m.getPhone() << ",,"
+                    << m.getCreditPoint() << ",,"
+                    << m.getOccupierRating() << ",,"
+                    << m.getHouse().getId() << endl;
+            }
+        }
     }
     myfile << "[Member Data End]" << endl;
     myfile << "--------------------------------------------------------------------------" << endl;
 
     myfile << "[House Data Start]" << endl;
     myfile << "id,,location,,description,,houseRating,,minOccupantScore,,consumingPoint,,startDate,,endDate,,occupyStatus" << endl;
+    v.clear();
     for (auto &h : this->houseList)
     {
-        myfile << h.getId() << ",,"
-               << h.getLocation() << ",,"
-               << h.getDescription() << ",,"
-               << h.getHouseRating() << ",,"
-               << h.getMinOccupantScore() << ",,"
-               << h.getConsumingPoint() << ",,"
-               << h.getStartDate() << ",,"
-               << h.getEndDate() << ",,"
-               << h.getOccupyStatus() << endl;
+        v.push_back(h.getId());
+    }
+    sort(v.begin(), v.end());
+    for (auto &n : v)
+    {
+        for (auto &h : this->houseList)
+        {
+            if (n == h.getId())
+            {
+                myfile << h.getId() << ",,"
+                       << h.getLocation() << ",,"
+                       << h.getDescription() << ",,"
+                       << h.getHouseRating() << ",,"
+                       << h.getMinOccupantScore() << ",,"
+                       << h.getConsumingPoint() << ",,"
+                       << h.getStartDate() << ",,"
+                       << h.getEndDate() << ",,"
+                       << h.getOccupyStatus() << endl;
+            }
+        }
     }
     myfile << "[House Data End]" << endl;
     myfile << "--------------------------------------------------------------------------" << endl;
 
     myfile << "[Request Data Start]" << endl;
     myfile << "id,,occupantId,,houseId,,houseRating,,occupantRating,,houseReview,,occupantReview,,requestStatus" << endl;
+    v.clear();
     for (auto &r : this->requestList)
     {
-        myfile << r.getId() << ",,"
-               << r.getOccupant().getId() << ",,"
-               << r.getHouse().getId() << ",,"
-               << r.getHouseRating() << ",,"
-               << r.getOccupantRating() << ",,"
-               << r.getHouseReview() << ",,"
-               << r.getOccupantReview() << ",,"
-               << r.getRequestStatus() << endl;
+        v.push_back(r.getId());
+    }
+    sort(v.begin(), v.end());
+    for (auto &n : v)
+    {
+        for (auto &r : this->requestList)
+        {
+            if (n == r.getId())
+            {
+                myfile << r.getId() << ",,"
+                       << r.getOccupant().getId() << ",,"
+                       << r.getHouse().getId() << ",,"
+                       << r.getHouseRating() << ",,"
+                       << r.getOccupantRating() << ",,"
+                       << r.getHouseReview() << ",,"
+                       << r.getOccupantReview() << ",,"
+                       << r.getRequestStatus() << endl;
+            }
+        }
     }
     myfile << "[Request Data End]" << endl;
     myfile << "--------------------------------------------------------------------------" << endl;
@@ -429,7 +466,7 @@ void Data::writeDataToFile(string filename)
     myfile.close();
 }
 
-void Data::readDataFromFile(string filename)
+void Data::readDataFromFile(string filename) // Read and extract data from file -> update to data
 {
     int countTemp = 0;
     bool isMember = false;
@@ -647,4 +684,61 @@ void Data::readDataFromFile(string filename)
         countTemp++;
     }
     myfile2.close();
+}
+
+void Data::updateHouseRating() // calculate average point of all rating towards one house, applied for all houses -> then update to data
+{
+    double sum = 0;
+    double count = 0;
+    House h;
+    for (auto &r : this->requestList)
+    {
+        if ((r.getHouseRating() != 0 || r.getHouseReview().compare(" ") != 0) && r.getRequestStatus() == true) // has been rated
+        {
+            sum = 0;
+            count = 0;
+            for (auto &req : this->requestList)
+            {
+                if ((req.getHouseRating() != 0 || req.getHouseReview().compare(" ") != 0) && req.getRequestStatus() == true) // has been rated
+                {
+                    if (r.getHouse().getId() == req.getHouse().getId())
+                    {
+                        sum += req.getHouseRating();
+                        count++;
+                    }
+                    h = r.getHouse();
+                    h.setHouseRating(sum / count);
+                    updateHouse(h);
+                }
+            }
+        }
+    }
+}
+void Data::updateOccupantRating() // calculate average point of all rating towards one occupant, applied for all occupants -> then update to data
+{
+    double sum = 0;
+    double count = 0;
+    Member m;
+    for (auto &r : this->requestList)
+    {
+        if ((r.getOccupantRating() != 0 || r.getOccupantReview().compare(" ") != 0) && r.getRequestStatus() == true) // has been rated
+        {
+            sum = 0;
+            count = 0;
+            for (auto &req : this->requestList)
+            {
+                if ((req.getOccupantRating() != 0 || req.getOccupantReview().compare(" ") != 0) && req.getRequestStatus() == true) // has been rated
+                {
+                    if (r.getOccupant().getId() == req.getOccupant().getId())
+                    {
+                        sum += req.getOccupantRating();
+                        count++;
+                    }
+                    m = r.getOccupant();
+                    m.setOccupierRating(sum / count);
+                    updateMember(m);
+                }
+            }
+        }
+    }
 }
--- Data.h
+++ Data.h
@@ -32,40 +32,47 @@ public:
     void setHouseList(vector<House> houseList);
     void setMemberList(vector<Member> memberList);
 
-    void addHouse(House house);
-    void addMember(Member member);
-    void addRequest(Request request);
+    void addHouse(House house);       // add new house to data: houseList
+    void addMember(Member member);    // add new member to data: memberList
+    void addRequest(Request request); // add new request to data: requestList
 
-    void removeHouse(House house);
-    void removeMember(Member member);
-    void removeRequest(Request request);
+    void removeHouse(House house);       // remove house from data: houseList
+    void removeMember(Member member);    // remove member from data: memberList
+    void removeRequest(Request request); // remove request from data: requestList
 
-    void updateHouse(House house);
-    void updateMember(Member member);
-    void updateRequest(Request request);
+    void updateHouse(House house);    // update house to data: houseList, add new one if not exist
+    void updateMember(Member member); // update member to data: memberList, add new one if not exist
+    void updateRequest(Request request); // update request to data: requestList, add new one if not exist
 
-    House getHouseById(int id);
-    Member getMemberById(int id);
-    Request getRequestById(int id);
-    Member getMemberByUsername(string username);
-    Member getMemberByHouseId(int id);
-    bool verifyMemberByUsername(string username);                             
-    bool verifyMemberByUsernameAndPassword(string username, string password); 
-    bool verifyHouseById(int id);
-    bool verifyRequestById(int id);
+    House getHouseById(int id);                  // return house of data: houseList by id
+    Member getMemberById(int id);                // return member of data: memberList by id
+    Request getRequestById(int id);              // return request of data: requestList by id
+    Member getMemberByUsername(string username); // return member of data: memberList by username
+    Member getMemberByHouseId(int id);           // return member of data: memberList by Houseid
 
-    Request createRequestToOccupy(Member occupant, int houseId);
-    vector<Request> deleteRequestFromRequestList(Request request, vector<Request> requests);
+    bool verifyMemberByUsername(string username);                             // check if member is in data: memberList with username
+    bool verifyMemberByUsernameAndPassword(string username, string password); // check if member is in data:memberList with username and password
+    bool verifyHouseById(int id);                                             // check if house is in data:houseList with id
+    bool verifyRequestById(int id);                                           // check if request is in data:requestList with id
 
-    void deleteHouseById(int id);
-    void deleteOtherRequestAfterAccept(Request request, vector<Request> requests);
-    void deleteRequestByHouseId(int id);
-    void ViewAllMembers();
-    void ViewAllHouses();
-    void ViewAllRequests();
+    Request createRequestToOccupy(Member occupant, int houseId); // create new request with input occupant and houseId
 
-    void writeDataToFile(string filename);
-    void readDataFromFile(string filename);
+    vector<Request> deleteRequestFromRequestList(Request request, vector<Request> requests); // return list of requests with the input request deleted
+
+    void deleteOtherRequestAfterAccept(Request request, vector<Request> requests); // delete all other requests in a request list, but the input request
+
+    void deleteHouseById(int id);        // delete house in data: houseList by Id
+    void deleteRequestByHouseId(int id); // delete request in data: requestList by Id
+    void ViewAllMembers();               // print out all members in data: memberList
+
+    void ViewAllHouses();   // print out all houses in data: houseList
+    void ViewAllRequests(); // print out all requests in data: requestList
+
+    void writeDataToFile(string filename);  // write data to file, with divided section for house, member and request, data is presented with sorted id
+    void readDataFromFile(string filename); // Read and extract data from file -> update to data
+
+    void updateHouseRating();    // calculate average point of all rating towards one house, applied for all houses -> then update to data
+    void updateOccupantRating(); // calculate average point of all rating towards one occupant, applied for all occupants -> then update to data
 };
 
 #endif
